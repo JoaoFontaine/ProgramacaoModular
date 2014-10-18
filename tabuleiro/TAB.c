95bcd01e1398b7764aa0cb06e80c412952006c4c
JoaoFontaine <joaopfcarvalho@hotmail.com>
2014-10-18 14:53:47 -0300

    Funcoes modulo tabuleiro que faltavam(incompleto)
    Coisas já feitas no módulo tabuleiro, ainda falta terminar a função
    MoverPeca e escrever todas as funções de movimentação específicas.

--- tabuleiro/TAB.c
+++ tabuleiro/TAB.c
@@ -9,23 +9,23 @@
 typedef struct TAB_tagPeca {
 
 	char nome;
-	    /* Nome da peca */
+	/* Nome da peca */
 	char cor;
-		/* Cor da peca */
+	/* Cor da peca */
 
 } tpPeca ;
-	
+
 
 typedef struct TAB_tagCasa {
 
-    tpPeca *  Peca; 
-		/* Ponteiro para a peca contida na casa */
+	tpPeca *  Peca; 
+	/* Ponteiro para a peca contida na casa */
 
 	LIS_tppLista  pAmeacadas;
-		/* Ponteiro para a cabeca da lista de ameacadas */
+	/* Ponteiro para a cabeca da lista de ameacadas */
 
 	LIS_tppLista  pAmeacantes;
-		/* Ponteiro para a cabeca da lista de ameacantes */
+	/* Ponteiro para a cabeca da lista de ameacantes */
 
 }tpCasa ;
 
@@ -34,25 +34,38 @@ typedef struct TAB_tagCasa {
 typedef struct TAB_tagLinhas {
 
 	LIS_tppLista  pLinha[LINHAS];
-		/* Vetor de ponteiros para a cabeca da lista de linhas */
+	/* Vetor de ponteiros para a cabeca da lista de linhas */
 
 }tpLinhas;
 
 
 
 typedef struct TAB_tagTab {
-	
+
 	tpLinhas * pLinhas;
-		/* Ponteiro para o vetor de ponteiros de linhas */
+	/* Ponteiro para o vetor de ponteiros de linhas */
 
 }TAB_tpTab;
 
 
 /***** Protótipos das funções encapuladas no módulo *****/
 
+TAB_tpCondRet ConfereCasaValida(int linha , char coluna, TAB_tppTab pTab);
+tpCasa * ObterCasa(int linha , char coluna, TAB_tppTab pTab);
 
-	
+TAB_tpCondRet ConfereMovimentoRetoValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest);
+TAB_tpCondRet ConfereMovimentoDiagonalValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest);
+TAB_tpCondRet ConfereMovimentoCavaloValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest);
 
+TAB_tpCondRet ConferePercursoVazio(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab);
+TAB_tpCondRet ConfereCaptura(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab);
+
+TAB_tpCondRet MoverPeao ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+TAB_tpCondRet MoverTorre ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+TAB_tpCondRet MoverBispo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+TAB_tpCondRet MoverCavalo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+TAB_tpCondRet MoverDama ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+TAB_tpCondRet MoverRei ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
@@ -78,10 +91,10 @@ TAB_tppTab CriarTab ( void ){
 
 			tpCasa * pCasa = ( tpCasa * ) malloc( sizeof( tpCasa )) ;
 			if ( pTab == NULL ) {  
-		    printf("\n Faltou memória para criar peca");
-		    return NULL;
-	        }
-	        /*if*/
+				printf("\n Faltou memória para criar peca");
+				return NULL;
+			}
+			/*if*/
 			pCasa->Peca->nome = 'V';
 			pCasa->Peca->cor = 'V';
 			pCasa->pAmeacadas = LIS_CriarLista( NULL );
@@ -97,7 +110,7 @@ TAB_tppTab CriarTab ( void ){
 	}
 	/*for*/
 
-	
+
 	return pTab;
 
 }
@@ -113,10 +126,10 @@ TAB_tppPeca ObterPeca ( int linha , char coluna, TAB_tppTab pTab ){
 	}
 	/*if*/
 	for( i=0; i<=(coluna - 'A'); i++){
-		LIS_IrProx (pTab->pLinhas->pLinha[i]);
+		LIS_IrProx (pTab->pLinhas->pLinha[linha-1]);
 	}
 	/*for*/
-	pCasa = (tpCasa *) LIS_ObterNo( pTab->pLinhas->pLinha[i] );
+	pCasa = (tpCasa *) LIS_ObterNo( pTab->pLinhas->pLinha[linha-1] );
 	if(pCasa->Peca->nome == 'V'){
 		printf("\n Casa Vazia");
 		return NULL;
@@ -124,7 +137,104 @@ TAB_tppPeca ObterPeca ( int linha , char coluna, TAB_tppTab pTab ){
 	/*if*/
 	return pCasa->Peca;
 }
-	
+
+TAB_tpCondRet RetirarPeca ( int linha , char coluna, TAB_tppTab pTab ){
+
+	TAB_tppPeca peca;
+
+	if( pTab == NULL){
+		return TAB_CondRetNaoExiste;
+	}
+	/*if*/
+	peca= ObterPeca ( linha , coluna, pTab );
+
+	if(peca == NULL){
+		return TAB_CondRetCasaVazia;
+	}
+
+	peca->cor= 'V';
+	peca->nome= 'V';
+
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverPeca ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+
+	TAB_tppPeca peca;
+	tpCasa * pCasaOrig;
+	tpCasa * pCasaDest;
+
+	if( pTab == NULL){
+		return TAB_CondRetNaoExiste;
+	}
+	/*if*/
+	if( ConfereCasaValida(linhaOrig, colunaOrig, pTab) != TAB_CondRetOK ){
+		return TAB_CondRetCasaInvalida;
+	}
+	/*if*/
+	if( ConfereCasaValida(linhaDest, colunaDest, pTab) != TAB_CondRetOK ){
+		return TAB_CondRetCasaInvalida;
+	}
+	/*if*/
+	peca= ObterPeca ( linhaOrig , colunaOrig, pTab );
+
+	if(peca == NULL){
+		return TAB_CondRetCasaVazia;
+	}
+	/*if*/
+
+	switch (peca->nome)
+	{
+	case 'P':
+		TAB_tpCondRet MoverPeao ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	case 'T':
+		TAB_tpCondRet MoverTorre ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	case 'B':
+		TAB_tpCondRet MoverBispo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	case 'C':
+		TAB_tpCondRet MoverCavalo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	case 'D':
+		TAB_tpCondRet MoverDama ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	case 'R':
+		TAB_tpCondRet MoverRei ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
+		break;
+	default:
+		break;
+	}
+
+}
+
+TAB_tpCondRet InserirPeca ( int linha , char coluna, TAB_tppPeca pPeca, TAB_tppTab pTab ){
+
+	tpCasa* casa;
+
+	if( pTab == NULL){
+		return TAB_CondRetNaoExiste;
+	}
+	/*if*/
+
+	if( ConfereCasaValida(linha, coluna, pTab) != TAB_CondRetOK ){
+		return TAB_CondRetCasaInvalida;
+	}
+	/*if*/
+
+	casa= ObterCasa(linha, coluna, pTab);
+
+	if(casa->Peca->nome != NULL){
+		return TAB_CondRetCasaCheia;
+	}
+
+	casa->Peca->nome= pPeca->nome;
+	casa->Peca->cor= pPeca->cor;
+
+	return TAB_CondRetOK;
+
+}
 
 LIS_tppLista ObterListaAmeacantes( int linha , char coluna, TAB_tppTab pTab ){
 
@@ -184,4 +294,169 @@ TAB_tpCondRet DestruirTab ( TAB_tppTab pTab ){
 	free(pTab);
 }
 
-	
+
+/* Funções Encapsuladas no módulo */
+
+
+TAB_tpCondRet ConfereCasaValida(int linha , char coluna, TAB_tppTab pTab){
+
+	int numColuna= int(coluna-'A');
+
+	if( 0 <= linha && linha < LINHAS ){
+		return TAB_CondRetCasaInvalida;
+	}
+
+	if( (0 <= numColuna) && (numColuna < COLUNAS) ){
+		return TAB_CondRetCasaInvalida;
+	}
+
+	return TAB_CondRetOK ;
+}
+
+
+tpCasa * ObterCasa(int linha , char coluna, TAB_tppTab pTab){
+
+	int i;
+	tpCasa * pCasa;
+
+	for( i=0; i<=(coluna - 'A'); i++){
+		LIS_IrProx (pTab->pLinhas->pLinha[linha-1]);
+	}
+	/*for*/
+	pCasa = (tpCasa *) LIS_ObterNo( pTab->pLinhas->pLinha[linha-1] );
+
+	return pCasa;
+}
+
+TAB_tpCondRet ConfereMovimentoRetoValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest){
+
+	int distanciaColunas= (int) colunaDest-colunaOrig;
+	int distanciaLinhas= linhaDest-linhaOrig;
+
+	if(distanciaColunas == distanciaLinhas){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	if(distanciaColunas != 0 && distanciaLinhas != 0){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet ConfereMovimentoDiagonalValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest){
+
+	int distanciaColunas= (int) colunaDest-colunaOrig;
+	int distanciaLinhas= linhaDest-linhaOrig;
+
+	if(distanciaColunas != distanciaLinhas){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	if(distanciaColunas == 0 && distanciaLinhas == 0){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet ConfereMovimentoCavaloValido(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest){
+
+	int distanciaColunas= (int) colunaDest-colunaOrig;
+	int distanciaLinhas= linhaDest-linhaOrig;
+
+	if( (abs(distanciaColunas) + abs(distanciaLinhas)) != 3){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	if(distanciaColunas == 0 || distanciaLinhas == 0){
+		return TAB_CondRetMovInv;
+	}
+	/*if*/
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet ConferePercursoVazio(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab){
+
+	int i;
+	int distanciaColunas= (int) colunaDest-colunaOrig;
+	int distanciaLinhas= linhaDest-linhaOrig;
+	int primeiraLinha= linhaOrig;
+	int primeiraColuna= (int) colunaOrig - 'A';
+
+	if(distanciaLinhas == 0){
+		if (distanciaColunas < 0){
+			primeiraColuna= (int) colunaDest - 'A';
+		}
+		for (i = 1; i < (primeiraColuna+distanciaColunas); i++){
+			if (ObterPeca(primeiraLinha,(char)(primeiraColuna+i + 'A'),pTab) != NULL){
+				return TAB_CondRetPecaBloqueando;
+			}
+		}
+
+	}
+	else if(distanciaColunas == 0){
+		if (distanciaLinhas < 0){
+			primeiraLinha= linhaDest;
+		}
+		for (i = 1; i < (primeiraLinha+distanciaLinhas); i++){
+			if (ObterPeca(primeiraLinha+i,(char)(primeiraColuna + 'A'),pTab) != NULL){
+				return TAB_CondRetPecaBloqueando;
+			}
+		}
+
+	}
+	else{
+		if (distanciaLinhas < 0){
+			primeiraColuna= (int) colunaDest - 'A';
+		}
+		if (distanciaColunas < 0){
+			primeiraColuna= colunaDest;
+		}
+		for (i = 1; i < (primeiraColuna+distanciaColunas); i++){
+			if (ObterPeca(primeiraLinha+i,(char)(primeiraColuna+i + 'A'),pTab) != NULL){
+				return TAB_CondRetPecaBloqueando;
+			}
+		}
+	}
+
+	return ConfereCaptura(linhaOrig, colunaOrig, linhaDest , colunaDest, pTab);
+}
+
+TAB_tpCondRet ConfereCaptura(int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab){
+
+	TAB_tppPeca pecaOrig= ObterPeca(linhaDest,colunaDest,pTab);
+	TAB_tppPeca pecaDest= ObterPeca(linhaDest,colunaDest,pTab);
+
+	if ( pecaDest != NULL){
+		if(pecaOrig->cor == pecaDest->cor){
+			return TAB_CondRetPecaBloqueando;
+		}
+		return TAB_CondRetCaptPeca;
+	}
+
+	return TAB_CondRetOK;
+}
+TAB_tpCondRet MoverPeao ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverTorre ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverBispo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverCavalo ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverDama ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
+
+TAB_tpCondRet MoverRei ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab ){
+	return TAB_CondRetOK;
+}
--- tabuleiro/TAB.h
+++ tabuleiro/TAB.h
@@ -6,35 +6,41 @@ typedef struct TAB_tagPeca * TAB_tppPeca;
 
 typedef enum {
 
-         TAB_CondRetOK = 0,
-               /* Concluiu corretamente */
+	TAB_CondRetOK = 0,
+	/* Concluiu corretamente */
 
-         TAB_CondRetNaoExiste = 1 ,
-               /* O tabuleiro nao existe */
+	TAB_CondRetNaoExiste = 1 ,
+	/* O tabuleiro nao existe */
 
-         TAB_CondRetCasaVazia = 2,
-			   /* A casa nao contem peca */
+	TAB_CondRetCasaVazia = 2,
+	/* A casa nao contem peca */
 
-		 TAB_CondRetMovInv = 3,
-			   /* Movimento de peca invalido */
+	TAB_CondRetMovInv = 3,
+	/* Movimento de peca invalido */
 
-		 TAB_CondRetCaptPeca = 4,
-			   /* Capturou peca inimiga */
+	TAB_CondRetCaptPeca = 4,
+	/* Capturou peca inimiga */
 
-         TAB_CondRetCasaCheia = 5
-		       /* A casa ja contem peca */
+	TAB_CondRetCasaCheia = 5,
+	/* A casa ja contem peca */
 
-   } TAB_tpCondRet ;
+	TAB_CondRetCasaInvalida = 6,
+	/* A casa nao pertence ao tabuleiro */
+
+	TAB_CondRetPecaBloqueando = 7
+	/* A casa nao pertence ao tabuleiro */
+
+} TAB_tpCondRet ;
 
 TAB_tppTab CriarTab ( void );
 
 TAB_tppPeca ObterPeca ( int linha , char coluna, TAB_tppTab pTab );
 
-TAB_tpCondRet RetirarPeca ( int linha , char coluna, TAB_tppPeca pPeca );
+TAB_tpCondRet RetirarPeca ( int linha , char coluna, TAB_tppTab pTab );
 
-TAB_tpCondRet MoverPeca ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest );
+TAB_tpCondRet MoverPeca ( int linhaOrig , char colunaOrig, int linhaDest , char colunaDest, TAB_tppTab pTab );
 
-TAB_tpCondRet InserirPeca ( int linha , char coluna, TAB_tppPeca pPeca );
+TAB_tpCondRet InserirPeca ( int linha , char coluna, TAB_tppPeca pPeca, TAB_tppTab pTab );
 
 LIS_tppLista ObterListaAmeacantes( int linha , char coluna, TAB_tppTab pTab );
 
