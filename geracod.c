7c8d5de6d73149af0598c476d26ebaae1c1ad803
JoaoFontaine <joaopfcarvalho@hotmail.com>
2014-11-16 13:48:07 -0200

    Uniao de tudo e correcoes

--- geracod.c
+++ geracod.c
@@ -4,36 +4,140 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include "geracod.h"
 
 #define NUM_MAX 1000
 
-typedef int (*funcp) ();
+static unsigned char comeco[]={0x55,0x89,0xe5,0x83,0xec,0x14}; /* salva o ebx e aloca espaco para 5 variaveis locais */
 
-static unsigned char comeco[]={0x55,0x89,0xe5,0x53,0x83,0xec,0x14}; /* salva o ebx e aloca espaco para 5 variaveis locais */
-
-static unsigned char final[]={0x83,0xc4,0x14,0x5b,0x89,0xec,0x5d,0xc3};  /* libera o ebx e o espaco das 5 variaveis locais */
+static unsigned char final[]={0x83,0xc4,0x14,0x89,0xec,0x5d,0xc3}; /* libera o ebx e o espaco das 5 variaveis locais */
 
 static unsigned char marcadorDesvio[]={0x39,0xc8,0x74,0x00};
 
-
 static void error (const char *msg, int line) {
-  fprintf(stderr, "erro %s na linha %d\n", msg, line);
-  exit(EXIT_FAILURE);
+	fprintf(stderr, "erro %s na linha %d\n", msg, line);
+	exit(EXIT_FAILURE);
+}
+
+void checkVar(char var, int idx, int line);
+int uniVet (unsigned char vet1[], unsigned char vet2[], int posicao, int tamVet2);
+void calculaDesvio(unsigned char codigo[], int desvionum[], int desviolinha[], int desvioposicao[], int enderecosnum[], int enderecoslinha[]);
+int Atribuicao (unsigned char *codigo, int posicao, char var0, int idx0, char var1, int idx1, char op, char var2, int idx2);
+int DesvioCondicional(unsigned char *codigo, int posicao, char var0, int idx0, char var1, int idx1);
+
+funcp geracod (FILE *myfp){
+	unsigned char *codigoRet;
+	unsigned char *codigo = (unsigned char*)malloc(NUM_MAX*(sizeof(unsigned char)));
+	int posicao = 0;
+	int line = 1;
+	int c;
+	int indice;
+	int i;
+	int desvionum[50];			/*armazena os numeros para onde ser√£o desviados */
+	int desviolinha[50];		/*armazena a linha em que h√° o comando de desvio */
+	int desvioposicao[50];		/*armazena a posicao no veto de codigo onde o desvio sera colocado */
+	int enderecosnum[50];		/*armazena os enderecos dos comandos*/
+	int enderecoslinha[50];		/*armazena as linhas do comando  */
+	int idx0, idx1, idx2, num;
+	char var0, var1, var2, op;
+	unsigned char aux[] = {0x8b, 0x45};
+	posicao = uniVet(codigo, comeco, posicao, 6);
+	
+	for(i=0;i<50;i++){
+		desviolinha[i]=0;
+	}
+	i=0;
+	
+	while ((c = fgetc(myfp)) != EOF) {
+
+		enderecosnum[line-1] = posicao;  		/*armazena os enderecos dos comandos*/
+		enderecoslinha[line-1] = line;			/*armazena as linhas do comando  */
+
+		switch (c) {
+		case 'r':   /* retorno */
+			
+			if (fscanf(myfp, "et %c%d", &var0, &idx0) != 2) 
+				error("comando invalido", line);
+			if (var0 != '$') checkVar(var0, idx0, line);
+
+			switch (var0) {
+			case '$': 
+				codigo[posicao++] = 0xb8;
+				codigo[posicao++] = (unsigned char) ( idx0 );
+				codigo[posicao++] = (unsigned char) ( idx0 >> 8 );
+				codigo[posicao++] = (unsigned char) ( idx0 >> 16 );
+				codigo[posicao++] = (unsigned char) ( idx0 >> 24 );
+				/*	codigo[posicao] = idx;   */
+
+				break;
+
+			case 'v': case 'p':
+				indice = ( var0 == 'p' ) ? (idx0+2)*4 : -(idx0+1)*4;
+				posicao = uniVet(codigo, aux, posicao, 2);
+				codigo[posicao++] = (unsigned char) indice;
+				break;
+
+			}
+			posicao = uniVet(codigo,final,posicao,7);
+			break;
+
+
+		case 'i': /* ifeq */
+			if (fscanf(myfp, "feq %c%d %c%d %d", &var0, &idx0, &var1, &idx1, 
+				&num) != 5)
+				error("comando invalido i",line);
+			if (var0 != '$') checkVar(var0, idx0, line);
+			if (var1 != '$') checkVar(var1, idx1, line);
+			printf("ifeq %c%d %c%d %d\n", var0, idx0, var1, idx1, num);
+
+			desvionum[i]=num;      /*armazena os numeros dos desvios */
+			desviolinha[i]=line;   /*armazena a linha em que h√° o comando de desvio */
+			posicao = DesvioCondicional(codigo, posicao, var0, idx0, var1, idx1);
+			posicao = uniVet(codigo,marcadorDesvio,posicao,4);
+			desvioposicao[i++] = posicao-1;
+			break;
+
+		case 'v': case 'p':  /* atribuicao */
+			var0 = c;
+			if (fscanf(myfp, "%d := %c%d %c %c%d",
+				&idx0, &var1, &idx1, &op, &var2, &idx2) != 6)
+				error("comando invalido", line);
+
+			checkVar(var0, idx0, line);
+			if (var1 != '$') {checkVar(var1, idx1, line);}
+			if (var2 != '$') {checkVar(var2, idx2, line);}
+			if (op != '+' && op != '-' && op != '*') {error("operacao invalida na atribuicao", line);}
+
+			posicao = Atribuicao(codigo, posicao, var0, idx0, var1, idx1, op, var2, idx2);
+			break;
+
+		default: error("comando desconhecido", line);
+		}
+		line ++;
+		fscanf(myfp, " ");
+	}	
+	posicao = uniVet(codigo,final,posicao,7);
+	codigo[posicao++] = 0xFF;
+	calculaDesvio(codigo,desvionum,desviolinha,desvioposicao,enderecosnum,enderecoslinha);
+	codigoRet = (unsigned char*)malloc(posicao*(sizeof(unsigned char)));
+	uniVet(codigoRet,codigo,0,posicao);
+	free(codigo);
+	return (funcp)codigoRet;
 }
 
 void checkVar(char var, int idx, int line) {
-  switch (var) {
-    case 'v':
-      if ((idx < 0) || (idx > 5))
-       error("operando invalido", line);
-      break;
-    case 'p':
-      if ((idx < 0) || (idx > 5))
-       error("operando invalido", line);
-      break;
-    default:
-       error("operando invalido", line);
-   }
+	switch (var) {
+	case 'v':
+		if ((idx < 0) || (idx > 5))
+			error("operando invalido", line);
+		break;
+	case 'p':
+		if ((idx < 0) || (idx > 5))
+			error("operando invalido", line);
+		break;
+	default:
+		error("operando invalido", line);
+	}
 }
 
 int uniVet (unsigned char vet1[], unsigned char vet2[], int posicao, int tamVet2){
@@ -51,12 +155,15 @@ void calculaDesvio(unsigned char codigo[], int desvionum[], int desviolinha[], i
 	int posicao;
 	int i;
 	for(i=0;i<50;i++){
-		if(desviolinha[i]!=0){
-			linhaDestino = desvionum[i];
+		if(desviolinha[i]!= 0){
+			linhaDestino = desvionum[i]-1;
 			linhaInicial = desviolinha[i];
 			diferencaDesvio = enderecosnum[linhaDestino]-enderecosnum[linhaInicial];
 			posicao = desvioposicao[i];
-			codigo[posicao]=diferencaDesvio;
+			printf("\n*****************\n%d\n",enderecosnum[linhaDestino]);
+			printf("%d\n",enderecosnum[linhaInicial]);
+			printf("%d\n*****************\n",diferencaDesvio);
+			codigo[posicao] = (unsigned char) diferencaDesvio;
 		}
 	}
 	/* Essa funcao vai procurar o comando de "je" no vetor de codigo e substituir o '0x00' pela diferenca entre os
@@ -66,164 +173,98 @@ void calculaDesvio(unsigned char codigo[], int desvionum[], int desviolinha[], i
 }
 
 
+int Atribuicao (unsigned char *codigo, int posicao, char var0, int idx0, char var1, int idx1, char op, char var2, int idx2){
+	int i = 0;
+	int indice;
+	unsigned char codAtribuicao[16];
 
-
-
-funcp geracod (FILE *f){
-
-	unsigned char * codigo = (unsigned char*)malloc(NUM_MAX*(sizeof(unsigned char)));
-	int posicao = 0;
-    int line = 1;
-    int  c;
-	int i;
-    FILE *myfp;
-	int desvionum[50];			/*armazena os numeros para onde ser„o desviados */
-	int desviolinha[50];		/*armazena a linha em que h· o comando de desvio */
-	int desvioposicao[50];		/*armazena a posicao no veto de codigo onde o desvio sera colocado */
-	int enderecosnum[50];		/*armazena os enderecos dos comandos*/
-	int enderecoslinha[50];		/*armazena as linhas do comando  */
-
-	posicao = uniVet(codigo, comeco, posicao, 7);
-	for(i=0;i<50;i++){
-		desviolinha[i]=0;
+	if ( var1 == '$' ){
+		codAtribuicao[i++] = 0xB8;// %eax
+		codAtribuicao[i++] = (char) ( idx1 );
+		codAtribuicao[i++] = (char) ( idx1 >> 8 );
+		codAtribuicao[i++] = (char) ( idx1 >> 16 );
+		codAtribuicao[i++] = (char) ( idx1 >> 24 );
 	}
-	i=0;
-
-	if ((myfp = fopen ("programa", "r")) == NULL){
-		perror ("nao conseguiu abrir arquivo!");
+	else{
+		indice = ( var1 == 'p' ) ? (idx1+2)*4 : -(idx1+1)*4;
+		codAtribuicao[i++] = 0x8B;
+		codAtribuicao[i++] = 0x45;// %eax
+		codAtribuicao[i++] = (char) indice;
 	}
-	while ((c = fgetc(myfp)) != EOF) {
 
-		enderecosnum[line-1]=(int) &codigo[posicao];  
-		enderecoslinha[line-1] = line;				
-
-		switch (c) {
-		  case 'r': {  /* retorno */
-			int idx;
-			char var;
-			if (fscanf(myfp, "et %c%d", &var, &idx) != 2) 
-				error("comando invalido", line);
-			if (var != '$') checkVar(var, idx, line);
-			printf("ret %c%d\n", var, idx);
-			
-			switch (var) {
-				case '$': {
-					codigo[posicao] = 0xb8;
-					posicao ++;
-				    codigo[posicao] = idx; /*DUVIDA*/
-					posicao=posicao+4;
-
-					break;
-					}
-				case 'p': {
-					unsigned char aux[] = {0x8b, 0x45};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao] = ((idx *4) + 8);
-					posicao ++;
-					break;
-					}
-				case 'v': {
-					unsigned char aux[] = {0x8b,0x45};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao]=(256-(idx*4)) ; /* deslocamento do ebp DUVIDA */
-					posicao++;
-					break ; 
-					break;
-					}
-			}
-			posicao = uniVet(codigo,final,posicao,8);
-			break;
-			}
-		
-		  case 'i': {  /* ifeq */
-			int idx0, idx1, num;
-			char var0, var1;
-			if (fscanf(myfp, "feq %c%d %c%d %d", &var0, &idx0, &var1, &idx1, 
-		                                        &num) != 5)
-			error("comando invalido i",line);
-			if (var0 != '$') checkVar(var0, idx0, line);
-			if (var1 != '$') checkVar(var1, idx1, line);
-			printf("ifeq %c%d %c%d %d\n", var0, idx0, var1, idx1, num);
-
-			desvionum[i]=num;      
-			desviolinha[i]=line;   
-			switch (var0) {
-				case '$': {
-					codigo[posicao] = 0xb9;
-					posicao++;
-					codigo[posicao] = idx0; /*DUVIDA*/
-					posicao=posicao+4;
-					break;
-					}
-
-				case'p':{
-					unsigned char aux[] = {0x8b,0x4d};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao] = ((idx0 *4) + 8);  
-					posicao ++;
-					break;
-					}
-				
-				case 'v':{
-					unsigned char aux[] = {0x8b,0x4d};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao]=(256-(idx1*4)) ; /* deslocamento do ebp DUVIDA */
-					posicao++;
-					break ; 
-					}
-			}
-			switch (var1){
-				case '$': {
-				    codigo[posicao] = 0xb8;
-					posicao++;
-					codigo[posicao] = idx0; /*DUVIDA*/
-					posicao=posicao+4;
-					break;
-					}
-
-				case'p':{
-					unsigned char aux[] = {0x8b,0x45};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao] = ((idx0 *4)+8); 
-					posicao ++;
-					break;
-					}
-				
-				case 'v':{
-					unsigned char aux[] = {0x8b,0x45};
-					posicao = uniVet(codigo,aux,posicao,2);
-					codigo[posicao]=(256-(idx1*4)) ; /* deslocamento do ebp DUVIDA */
-					posicao++;
-					break ; 
-					}
-			}
-			posicao = uniVet(codigo,marcadorDesvio,posicao,4);
-			desvioposicao[i] = posicao;
-			i++;
-			break;
-		  }
-
-		case 'v': case 'p': case '$': {  /* atribuicao */
-			int idx0, idx1, idx2;
-			char var0 = c, var1, var2;
-			char op;
-			if (fscanf(myfp, "%d %c %c%d -> %c%d",
-			           &idx0, &op, &var1, &idx1, &var2, &idx2) != 6)
-			  error("comando invalido", line);
-			if (var0 != '$') checkVar(var0, idx0, line);
-			if (var1 != '$') checkVar(var1, idx1, line);
-			checkVar(var2, idx2, line);
-			printf("%c%d %c %c%d -> %c%d\n",
-			        var0, idx0, op, var1, idx1, var2, idx2);
-
-			break;
-		}	
-		default: error("comando desconhecido", line);
-		}
-		line ++;
-		fscanf(myfp, " ");
-		}
-
-	calculaDesvio(codigo,desvionum,desviolinha,desvioposicao,enderecosnum,enderecoslinha);	
-	return (funcp)codigo;
+	if ( var2 == '$' ){
+		codAtribuicao[i++] = 0xB9;// %ecx
+		codAtribuicao[i++] = (char) ( idx2 );
+		codAtribuicao[i++] = (char) ( idx2 >> 8 );
+		codAtribuicao[i++] = (char) ( idx2 >> 16 );
+		codAtribuicao[i++] = (char) ( idx2 >> 24 );
 	}
+	else{
+		indice = ( var2 == 'p' ) ? (idx2+2)*4 : -(idx2+1)*4;
+		codAtribuicao[i++] = 0x8B;
+		codAtribuicao[i++] = 0x4D;// %ecx
+		codAtribuicao[i++] = (char) indice;
+	}
+
+	switch (op)
+	{
+	case '+':
+		codAtribuicao[i++] = 0x01;
+		codAtribuicao[i++] = 0xC8;
+		break;
+	case '-':
+		codAtribuicao[i++] = 0x29;
+		codAtribuicao[i++] = 0xC8;
+		break;
+	case '*':
+		codAtribuicao[i++] = 0x0F;
+		codAtribuicao[i++] = 0xAF;
+		codAtribuicao[i++] = 0xC1;
+		break;
+	default:
+		error("operacao invalida na atribuicao", -1);
+		break;
+	}
+
+	indice = ( var0 == 'p' ) ? (idx0+2)*4 : -(idx0+1)*4;
+	codAtribuicao[i++] = 0x89;
+	codAtribuicao[i++] = 0x45;// %eax
+	codAtribuicao[i++] = (char) indice;
+
+	return uniVet(codigo, codAtribuicao, posicao, i);
+}
+
+int DesvioCondicional(unsigned char *codigo, int posicao, char var0, int idx0, char var1, int idx1){
+	int i = 0 ;
+	int indice;
+	unsigned char codDesvio[16];
+	if ( var0 == '$' ){
+		codDesvio[i++] = 0xB8;// %eax
+		codDesvio[i++] = (char) ( idx0 );
+		codDesvio[i++] = (char) ( idx0 >> 8 );
+		codDesvio[i++] = (char) ( idx0 >> 16 );
+		codDesvio[i++] = (char) ( idx0 >> 24 );
+	}
+	else{
+		indice = ( var0 == 'p' ) ? (idx0+2)*4 : -(idx0+1)*4;
+		codDesvio[i++] = 0x8B;
+		codDesvio[i++] = 0x45;// %eax
+		codDesvio[i++] = (char) indice;
+	}
+
+	if ( var1 == '$' ){
+		codDesvio[i++] = 0xB9;// %ecx
+		codDesvio[i++] = (char) ( idx1 );
+		codDesvio[i++] = (char) ( idx1 >> 8 );
+		codDesvio[i++] = (char) ( idx1 >> 16 );
+		codDesvio[i++] = (char) ( idx1 >> 24 );
+	}
+	else{
+		indice = ( var1 == 'p' ) ? (idx1+2)*4 : -(idx1+1)*4;
+		codDesvio[i++] = 0x8B;
+		codDesvio[i++] = 0x4D;// %ecx
+		codDesvio[i++] = (char) indice;
+	}
+
+	return uniVet(codigo, codDesvio, posicao, i);
+}
--- mainTeste.c
+++ mainTeste.c
@@ -2,13 +2,15 @@
 #include<stdlib.h>
 #include "geracod.h"
 
-int main (void){
+void ImprimeCodigo(funcp pFun);
+
+int main (int argc, char *argv[]){
 
 	funcp pFun;
 	int resposta;
 	int i;
-	unsigned char *tst;
-	char* nFonte= "/home/joaofontaine/Dropbox/sb/trabalho2/arquivosTeste/tst01.txt";
+	int parametros[5] = {0,0,0,0,0};
+	char* nFonte= "/home/joaofontaine/Dropbox/sb/trabalho2/arquivosTeste/tst.txt";
 	FILE *arqEntrada;
 	
 	arqEntrada = fopen( nFonte, "r");
@@ -18,16 +20,29 @@ int main (void){
 	}
 	
 	pFun = geracod ( arqEntrada );
-	/*
-	tst = (unsigned char*) pFun;
-	for(i=0; i<35; i++){
-		printf("%0x\n", tst[i]);
-	}*/
-
-	resposta = (*pFun)(10,20,5);
+	if(argc>1)
+	{
+		if(argv[1][0] == '1'){
+			ImprimeCodigo(pFun);
+		}
+		for(i = 0; i < argc-2; i++){
+			parametros[i] = atoi(argv[i+2]);
+		}
+	}
+	
+	resposta = (*pFun)(parametros[0],parametros[1],parametros[2],parametros[3],parametros[4]);
 	
 	printf("resposta = %d\n", resposta);
 	
   	return 0;
 }
 
+void ImprimeCodigo(funcp pFun){
+	int i;
+	unsigned char *tst;
+	
+	tst = (unsigned char*) pFun;
+	for(i=0; tst[i] != 0xFF || tst[i-1] != 0xC3; i++){
+		printf("%0x\n", tst[i]);
+	}
+}
